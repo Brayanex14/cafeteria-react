305441e545da5c28619ae0b559fe855e18262c83
Brayanex14 <brayantuamigo64@hotmail.com>
2025-06-15 23:27:44 -0400

    cambio 2 en compras.jsx

--- src/Componentes/Compras/Compras.jsx
+++ src/Componentes/Compras/Compras.jsx
@@ -136,14 +136,24 @@ const Compras = ({ carrito2 = [] }) => {
             className="cuenta-boton"
           >
             <Container>
-              <Form.Group className="mb-3" controlId="formNombreTarjeta">
-                <Form.Label>Nombre del propietario:</Form.Label>
-                <Form.Control
-                  className="form-text"
-                  type="text"
-                  placeholder="Nombre completo"
-                />
-              </Form.Group>
+              <Row xs={1} sm={2} md={2} lg={2} className="mb-3">
+                <Col>
+                  <Form.Label>Nombre(s)</Form.Label>
+                  <Form.Control
+                    className="form-text"
+                    type="text"
+                    placeholder="Nombres"
+                  />
+                </Col>
+                <Col>
+                  <Form.Label>Apellido(s)</Form.Label>
+                  <Form.Control
+                    className="form-text"
+                    type="text"
+                    placeholder="Apellidos"
+                  />
+                </Col>
+              </Row>
 
               <Form.Group className="mb-3" controlId="formNumeroTarjeta">
                 <Form.Label>Número de cuenta:</Form.Label>
